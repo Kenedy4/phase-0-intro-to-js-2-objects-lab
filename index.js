4c07ad7113e915f83534e3505d68d3200ca0715e
kenedy4 <kennedymusyoki8@gmail.com>
2024-03-16 16:54:49 +0300

    learning Objects in Javascript

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ 
+    return { ...employee, [key]: value };
+  }
+  const employee = {
+    name: "Sam",
+   
+  };
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetaddress", "11 Broadway");
+console.log(updatedEmployee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
